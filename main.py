6bde7a7d7140582e06b6d00cec7249c3e3ac1caf
Julien Delannoy <julien.delannoy@hotmail.com>
2019-02-15 23:41:42 +0100

    gif generator

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import numpy as np
 import random
 import cv2
 from scipy import ndimage
-from time import sleep
+from time import sleep, time
+import os
+import glob
+import moviepy.editor as mpy
 
 def init():
     global canvaY, canvaX, cellBornMin, cellSolitude, cellBornMax, cellOverpop
-    canvaY = 80 # px
-    canvaX = 80 # px
+    canvaY = 50 # px
+    canvaX = 70 # px
     cellOverpop = 3
     cellBornMin = 2
     cellSolitude = cellBornMin
@@ -68,47 +71,84 @@ def mouseControl(event,x,y,flags,param):
         elif erasing:
             canva[y//zoom, x//zoom] = 0 
 
+def createGif(canvas):
+    directory='tmp'+str(int(time()))
+    os.mkdir(directory) 
+    canvasGif=[]
+    for element in canvas:
+        canvasGif.append(cv2.cvtColor(element,cv2.COLOR_GRAY2RGB))
+
+    i=0
+    for element in canvasGif:
+        cv2.imwrite('./'+directory+'/'+"{0:0=10d}".format(i)+'.png',element)
+        i+=1
+        
+    filenames = glob.glob("./"+directory+"/*.png")
+    filenames.sort()
+    clip = mpy.ImageSequenceClip(filenames, fps=10)
+    clip.write_gif('lifegame'+str(int(time()))+'.gif', fps=10)
+    os.rmdir(directory)
+    
 def nothing(*args):
     pass
 
 def main():
     try:
         init()
-        global canva, zoom, drawing, erasing
-
+        global canva, canvas, zoom, drawing, erasing
+        i=0
+        canvas=[]
         pause = True
+        save = False
         drawing, erasing = False, False
-        zoom = 900//canvaY
+        zoom = min(1700//canvaX, 900//canvaY)
         canva = createCanva()
         cv2.startWindowThread()
         cv2.namedWindow("life")
-        cv2.namedWindow("param")
         
         cv2.setMouseCallback('life',mouseControl)
-        cv2.createTrackbar('lift','param',0,5,nothing)
-        cv2.createTrackbar('sleepPeriod','param',0,500,nothing)
+        cv2.createTrackbar('lift','life',0,5,nothing)
+        cv2.createTrackbar('sleepPeriod','life',0,500,nothing)
+        cv2.setTrackbarPos('sleepPeriod','life',100)
         
         while True:
-            order = cv2.getTrackbarPos('lift','param')
-            sleepPeriod = cv2.getTrackbarPos('sleepPeriod','param')/1000
-            cv2.imshow('life', ndimage.zoom(canva, zoom=zoom,order=order)*255)
+            order = cv2.getTrackbarPos('lift','life')
+            sleepPeriod = cv2.getTrackbarPos('sleepPeriod','life')/1000
             
+            canvaPic=ndimage.zoom(canva, zoom=zoom,order=order)*255
+            
+            if save:
+                canvas.append(canvaPic)
+                cv2.circle(canvaPic,(canvaPic.shape[1]-40,40), 20, (100), -1)
+            
+            cv2.putText(canvaPic,str(i),(20,50), cv2.FONT_HERSHEY_SIMPLEX, 1,(255),3)
+            
+            cv2.imshow('life', canvaPic)
+
             k = cv2.waitKey(1) & 0xFF
             if k == ord('q'):
                 cv2.destroyAllWindows()
                 break
             elif k == ord('r'):
+                i=0
                 canva = randomInit(canva)
             elif k == ord('i'):
+                i=0
                 canva = createCanva()
             elif k == ord('p'):
                 pause = not pause
-                
+            elif k == ord('s'):
+                save = not save
             if not pause or k == ord('n'): 
                 canva = applyNextGen(canva)
+                i+=1
             if not any([drawing,erasing]):
                 sleep(sleepPeriod)
+            
+        if len(canvas) != 0:
+            createGif(canvas)
     except Exception as error:
         print('Error: ' + str(type(error)) + ' - ' + str(error.args))
         cv2.destroyAllWindows()
+        
 main()
